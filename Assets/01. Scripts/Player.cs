9322dbaae7251677fca8af3157c08050af9d2e34
jws2170 <mpart2077@gmail.com>
2025-02-10 23:17:12 +0900

    추가 | 플레이어

---
+++ Assets/01. Scripts/Player.cs
@@ -0,0 +1,16 @@
+namespace Agent
+{
+    //System
+    using System.Collections;
+    using System.Collections.Generic;
+
+    //UnityEngine
+    using UnityEngine;
+
+    public class Player : Character
+    {
+        
+    }
+
+}
+
